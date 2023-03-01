54b8fa953ba5066f1fb8d4883aad4531ee3a2392
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-03-01 07:57:54 +0000

    Print welcome message

--- run.py
+++ run.py
@@ -12,8 +12,22 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("bedtime_adventures")
 
-story = SHEET.worksheet("story")
 
-data = story.get_all_values()
-print(data)
+# input names
 
+# start story with names from input
+
+# make a choice to continue story
+
+# make another choice to continue story. 
+ 
+# print end of story. 
+
+#def main():
+    
+
+print("Welcome to Bedtime Adventures!")
+print("Get ready to embark on exciting and imaginative journeys with your child. Our interactive stories are designed to encourage your child's creativity and critical thinking skills, while also providing an enjoyable and engaging experience.")
+print("Let's dive in and explore new worlds together!")
+
+#main()
