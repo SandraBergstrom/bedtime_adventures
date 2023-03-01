d04c58f1517526fba848883b7f7300829899f263
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-03-01 08:23:36 +0000

    Create get_user_names to get names for story

--- run.py
+++ run.py
@@ -13,7 +13,21 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("bedtime_adventures")
 
 
-# input names
+def get_user_names():
+    """
+    Get name input from user.
+    """
+    print("Who will be the heroes of tonight's adventure? We need two brave and adventurous names for our characters. \n")
+    print("If you are reading to more than one child, we suggest using their names, but if you're reading to a single child, you can add your own name as the second hero.")
+    print("Please type the two names, separated by a comma, and then press enter to get started with the story!")
+    print("Example: Bubbles, Giggles")
+
+    names = input("Enter names here: ")
+    print(f"The names proivded is {names}")
+
+    
+
+
 
 # start story with names from input
 
@@ -26,8 +40,11 @@ SHEET = GSPREAD_CLIENT.open("bedtime_adventures")
 #def main():
     
 
-print("Welcome to Bedtime Adventures!")
+print("Welcome to Bedtime Adventures! \n")
 print("Get ready to embark on exciting and imaginative journeys with your child. Our interactive stories are designed to encourage your child's creativity and critical thinking skills, while also providing an enjoyable and engaging experience.")
-print("Let's dive in and explore new worlds together!")
+print("Let's dive in and explore new worlds together! \n")
+
+get_user_names()
+
 
 #main()
