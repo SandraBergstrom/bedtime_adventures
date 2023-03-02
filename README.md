fdbfb64bbf5dfb3c3cf2a0610d7738ebcac23a27
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-03-02 11:56:14 +0000

    Add get_adventure_function and check validation

--- README.md
+++ README.md
@@ -10,7 +10,19 @@ Name validation:
 - 3 or more letters inserted - OK for both names
 - Only letters - OK for both names
 - Correct name input - OK for both names
+- Numbers instead of letters - OK, gives correct error message
+- 2 lettters inserted - OK, gives correct error message
+
+First choice validation:
+- X - OK
+- Y - OK
+- anything else inserted - 
 
 Bugs:
 - Name validation for only letters will cause an error everytime because of the csv.
     - Created 2 input fields so one for each name instaed. Changed so it validates that there is 3 or more letters in each name. 
+- At first choice when Y is pressed, error message: UnboundLocalError: local variable 'story' referenced before assignment.
+    - I had mistakenly added an extra = when calling the SHEET. 
+
+- When I have made my first choice it directly tells me again to make a choice even if I put in a valid value (X or Y)
+    - by putting a print(bug) before the first input in get_start_story and a print(bug2) before the second input in get_adventure_story I realized that it jumped straight to the input field in get_adventure_story after I have put in a valid value, but went back to the input field in the get_start_story if I put in a not valid value. Turnes out that I mistakenly have removed the print(story_text) in get_adventure_story so everything else was working as it should. 
--- run.py
+++ run.py
@@ -69,22 +69,62 @@ def validate_names(name):
 def get_start_story(name1, name2):
     """
     Get the beginning of the story from google sheets and replace [Name1] and [Name2]
-    with names from input. 
+    with names from input. Add adventure choice for user to pick how the story continues.
     """
+       
     story = SHEET.worksheet("story").get_all_values()
     story_text = "\n".join([row[0] for row in story])
     story_text = story_text.replace("[Name1]", name1)
     story_text = story_text.replace("[Name2]", name2)
     story_text = story_text.replace("'\\n'", "\n")
-    
+       
     print(story_text)
-    adventure_choice = input("Insert X or Y here: ")
-    choice = adventure_choice.capitalize()
-    validate_story_choice(choice)
-    return choice
+    while True: 
+        print("bug")
+        choice1 = input("Insert X or Y here: ")
+        choice1 = choice1.capitalize()
+        
+        if validate_story_choice(choice1):
+            print("Let's go!")
+            break
+
+    return choice1
+
+
+def get_adventure_story(name1, name2, choice1):
+    """
+    Get adventure story X or Y depending on choice in start story.
+    Add end choice for user to pick how the story ends. 
+    """
     
+    if choice1 == "X":
+        story = SHEET.worksheet("x").get_all_values()
+        story_text = "\n".join([row[0] for row in story])
+        story_text = story_text.replace("[Name1]", name1)
+        story_text = story_text.replace("[Name2]", name2)
+        story_text = story_text.replace("'\\n'", "\n")
+        print(story_text)
+                
+    elif choice1 == "Y":
+        story = SHEET.worksheet("y").get_all_values()
+        story_text = "\n".join([row[0] for row in story])
+        story_text = story_text.replace("[Name1]", name1)
+        story_text = story_text.replace("[Name2]", name2)
+        story_text = story_text.replace("'\\n'", "\n")
+        print(story_text)
+    
+    while True: 
+        print("bug2")        
+        choice2 = input("Insert X or Y here: ")
+        choice2 = choice2.capitalize()
 
+    
+        if validate_story_choice(choice2):
+            print("Let's go!")
+            break
 
+    return choice1 + choice2
+    
 def validate_story_choice(choice):
     """
     Validate that user enters either X or Y 
@@ -92,7 +132,7 @@ def validate_story_choice(choice):
     try:
         if choice != "X" and choice != "Y":
             raise ValueError(
-                f"You need to pick either X or Y, you wrote {choice}!"
+                f"You need to pick either X or Y, you wrote {choice} !"
             )
     except ValueError as e:
         print(f"Oopsie daisy! {e}. Try again!\n")
@@ -101,20 +141,15 @@ def validate_story_choice(choice):
     return True
 
 
-# make a choice to continue story
-
-# make another choice to continue story. 
- 
-# print end of story. 
-
-
 def main():
     name1 = get_user_name1()
     name2 = get_user_name2()
-    adventure = get_start_story(name1, name2)
-    print(adventure)
+    choice1 = get_start_story(name1, name2)
+    #print(choice1)
+    choice2 = get_adventure_story(name1, name2, choice1)
+    #print(choice2)
 
-    
+
 print("Welcome to Bedtime Adventures! \n")
 print("Get ready to embark on exciting and imaginative journeys with your child. Our interactive stories are designed to encourage your child's creativity and critical thinking skills, while also providing an enjoyable and engaging experience.\n")
 print("Let's dive in and explore new worlds together! \n")
